966656c15416a5a4717cef2d2430b2673e0bbd25
lxj <769240262@qq.com>
2020-11-02 17:02:13 +0800

    201102-登入成功后页面展示

--- src/main/java/com/lxj/controller/AuthorizeController.java
+++ src/main/java/com/lxj/controller/AuthorizeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Controller
@@ -30,7 +31,8 @@ public class AuthorizeController {
 
     @GetMapping("/oauth/callback")
     public String callback(@RequestParam(name = "code") String code,
-            @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -39,11 +41,19 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         try {
             String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-            GithubUser githubUser = githubProvider.getUser(accessToken);
-            System.out.println(githubUser);
+            GithubUser user = githubProvider.getUser(accessToken);
+            if(user != null){
+                //登入成功
+                request.getSession().setAttribute("user",user);
+                return "redirect:/index";
+            }else {
+                //登入失败
+                return "redirect:/index";
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return "";
+        return "index";
     }
 }
